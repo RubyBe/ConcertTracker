f03199f2dd4509573dbcf0c88cfe1797b22cf371
RubyBe <sidbenavente@hotmail.com>
2016-08-05 08:12:01 -0700

    add tests and methods for saving a record and returning all records

---
+++ Objects/Band.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BandTracker
+{
+    public class Band
+    {
+      // Properties
+      private int _id;
+      private string _name;
+
+      // Constructors, getters, setters
+      public Band(string name, int Id = 0)
+      {
+        _name = name;
+        _id = Id;
+      }
+      public int GetId()
+      {
+        return _id;
+      }
+      public string GetName()
+      {
+        return _name;
+      }
+      public void SetName(string name)
+      {
+        _name = name;
+      }
+      // Other methods
+      // a method to save an instance of the band object to the database
+      public void Save()
+      {
+        SqlConnection conn = DB.Connection();
+        conn.Open();
+
+        SqlCommand cmd = new SqlCommand("INSERT INTO bands (name) OUTPUT INSERTED.id VALUES (@BandName);", conn);
+
+        SqlParameter nameParameter = new SqlParameter();
+        nameParameter.ParameterName = "@BandName";
+        nameParameter.Value = this.GetName();
+        cmd.Parameters.Add(nameParameter);
+        SqlDataReader rdr = cmd.ExecuteReader();
+
+        while(rdr.Read())
+        {
+          this._id = rdr.GetInt32(0);
+        }
+        if (rdr != null)
+        {
+          rdr.Close();
+        }
+        if(conn != null)
+        {
+          conn.Close();
+        }
+      }
+      // a method to override Equality and test the equivalence of name and id instead
+      public override bool Equals(System.Object otherBand)
+      {
+        if (!(otherBand is Band))
+        {
+          return false;
+        }
+        else
+        {
+          Band newBand = (Band) otherBand;
+          bool idEquality = this.GetId() == newBand.GetId();
+          bool nameEquality = this.GetName() == newBand.GetName();
+          return (idEquality && nameEquality);
+        }
+      }
+      // a method to return all Band object instances in a list
+      public static List<Band> GetAll()
+      {
+        List<Band> allBands = new List<Band>{};
+        SqlConnection conn = DB.Connection();
+        conn.Open();
+        SqlCommand cmd = new SqlCommand("SELECT * FROM bands;", conn);
+        SqlDataReader rdr = cmd.ExecuteReader();
+
+        while(rdr.Read())
+        {
+          int bandId = rdr.GetInt32(0);
+          string bandName = rdr.GetString(1);
+          Band newBand = new Band(bandName, bandId);
+          allBands.Add(newBand);
+        }
+        if (rdr != null)
+        {
+          rdr.Close();
+        }
+        if (conn != null)
+        {
+          conn.Close();
+        }
+        return allBands;
+      }
+      // a method to delete all instances of Band
+      public static void DeleteAll()
+      {
+        SqlConnection conn = DB.Connection();
+        conn.Open();
+        SqlCommand cmd = new SqlCommand("DELETE FROM bands;", conn);
+        cmd.ExecuteNonQuery();
+        conn.Close();
+      }
+    }
+}
---
+++ Tests/BandTest.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Xunit;
+
+namespace BandTracker
+{
+  public class BandTest : IDisposable
+  {
+    public BandTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=band_tracker_test;Integrated Security=SSPI;";
+    }
+    // a method to delete records between tests
+    public void Dispose()
+    {
+      Band.DeleteAll();
+    }
+    [Fact]
+    public void Test_BandsEmptyAtFirst()
+    {
+      //Arrange, Act
+      int result = Band.GetAll().Count;
+      //Assert
+      Assert.Equal(0, result);
+    }
+    [Fact]
+    public void Test_Equal_ReturnsTrueForSameName()
+    {
+      //Arrange, Act
+      Band firstBand = new Band("The LoudMouths");
+      Band secondBand = new Band("The LoudMouths");
+      //Assert
+      Assert.Equal(firstBand, secondBand);
+    }
+    [Fact]
+    public void Test_Save_SavesBandToDatabase()
+    {
+      //Arrange
+      Band testBand = new Band("The LoudMouths");
+      testBand.Save();
+      //Act
+      List<Band> result = Band.GetAll();
+      List<Band> testList = new List<Band>{testBand};
+      //Assert
+      Assert.Equal(testList, result);
+    }
+  }
+}
